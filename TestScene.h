f44753cc309f3196514ef17b225edd04479b77ea
悠人 菅原 <220369@jc-21.jp>
2024-03-08 11:08:41 +0900

    プロジェクト ファイルを追加します。

---
+++ TestScene.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Engine/GameObject/GameObject.h"
+
+class TestScene : public GameObject
+{
+public:
+	TestScene(GameObject* parent);
+	void Initialize() override;
+	void Update() override;
+	void Draw() override;
+	void Release() override;
+};
